f712a87fb364c3bd566864eacd4b8910c5ab5b4d
kjmyeong <cptm2399@gmail.com>
2021-04-30 17:45:17 +0900

    김정명 가입

---
+++ lib.py
@@ -0,0 +1,4 @@
+def login(name):
+    with open(f"C:/project/Human_project.txt", mode="a", encoding="utf-8") as f:
+    # with open(f"C:/project/{name}.txt", mode="wt", encoding="utf-8") as f:
+        f.write(f"{name}님 반갑습니다.\n")
---
+++ main.py
@@ -0,0 +1,6 @@
+import lib
+
+
+name = input("이름을 입력해주세요: ")
+
+lib.login(name)
