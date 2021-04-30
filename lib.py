2015d5a3bfc0cf4437ddaa1e8529efc8ecaa1209
kjmyeong <cptm2399@gmail.com>
2021-04-30 18:34:29 +0900

    김정명 가입

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
 def login(name):
-    with open(f"C:/project/Project/Human_project.txt", mode="a", encoding="utf-8") as f:
+    with open(f"./Human_project.txt", mode="a", encoding="utf-8") as f:
     # with open(f"C:/project/{name}.txt", mode="wt", encoding="utf-8") as f:
         f.write(f"{name}님 반갑습니다.\n")
