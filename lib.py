07cc7a9dfa3655228bc450f377188152eb6e8867
hjss705 <hjss705@nate.com>
2021-05-02 21:41:22 +0900

    project

--- lib.py
+++ lib.py
@@ -3,3 +3,4 @@ def login(name):
     # with open(f"C:/project/{name}.txt", mode="wt", encoding="utf-8") as f:
         f.write(f"{name}님 반갑습니다.\n")
         f.write(f"{name}님 협곡으로 들어오세요\n")
+        f.write(f"{name}님 로또 1등되세요^^\n")
